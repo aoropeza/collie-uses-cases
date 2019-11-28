683c89da20420543c4c9baef3d45c1bffeef2557
Alejandro Oropeza <Aoropez>
2019-11-28 17:06:45 -0600

    Modified example ᕦ╏^^╏ᕤ

--- example/index.js
+++ example/index.js
@@ -19,86 +19,85 @@ usesCases(config)
   .then(({ addSource, bulkBrands, bulkLocations, bulkMovies }) => {
     console.log('EXAMPLE: ----> Uses cases gotten correctly')
 
-    return Promise.resolve().then(() =>
-      bulkMovies({
-        movies: [
-          {
-            name: 'Terminator'
-          },
-          {
-            name: 'The current war'
-          }
-        ]
-      })
-    )
-    /*
-        .then(() =>
-          addSource({
-            sources: [
-              {
-                ip: '127.0.0.1',
-                clientType: 'web',
-                clientName: 'chrome',
-                clientSo: 'mac',
-                clientVersion: '12.2.3'
-              }
-            ]
-          })
-        )
-      
-        .then(() =>
-          bulkBrands({
-            brands: [
-              {
-                name: 'Cinepolis',
-                logo: 'https://www.cinepolis.com/logo2.png'
-              },
-              {
-                name: 'Cinemex',
-                logo: 'https://www.cinemex.com/logo2.png'
-              }
-            ]
-          })
-        )
-        .then(() =>
-          bulkLocations({
-            brand: {
+    return Promise.resolve()
+      .then(() =>
+        bulkMovies({
+          movies: [
+            {
+              name: 'Terminator'
+            },
+            {
+              name: 'The current war'
+            }
+          ]
+        })
+      )
+      .then(() =>
+        addSource({
+          sources: [
+            {
+              ip: '127.0.0.1',
+              clientType: 'web',
+              clientName: 'chrome',
+              clientSo: 'mac',
+              clientVersion: '12.2.3'
+            }
+          ]
+        })
+      )
+
+      .then(() =>
+        bulkBrands({
+          brands: [
+            {
               name: 'Cinepolis',
               logo: 'https://www.cinepolis.com/logo2.png'
             },
-            locations: [
-              {
-                name: 'Plaza central',
-                latitude: 19.4574866,
-                longitude: 19.4574877,
-                address: 'Esquina calle norte 13 número 56'
-              },
-              {
-                name: 'Plaza sur',
-                latitude: 19.4574861,
-                longitude: 19.4574877,
-                address: 'Esquina calle sur 33 número 34'
-              }
-            ]
-          })
-        )
-        .then(() =>
-          bulkLocations({
-            brand: {
+            {
               name: 'Cinemex',
               logo: 'https://www.cinemex.com/logo2.png'
+            }
+          ]
+        })
+      )
+      .then(() =>
+        bulkLocations({
+          brand: {
+            name: 'Cinepolis',
+            logo: 'https://www.cinepolis.com/logo2.png'
+          },
+          locations: [
+            {
+              name: 'Plaza central',
+              latitude: 19.4574866,
+              longitude: 19.4574877,
+              address: 'Esquina calle norte 13 número 56'
             },
-            locations: [
-              {
-                name: 'Plaza oriente',
-                latitude: 19.4574855,
-                longitude: 19.457455,
-                address: 'Esquina calle oriente 66 número 34'
-              }
-            ]
-          })
-        )
-        */
+            {
+              name: 'Plaza sur',
+              latitude: 19.4574861,
+              longitude: 19.4574877,
+              address: 'Esquina calle sur 33 número 34'
+            }
+          ]
+        })
+      )
+      .then(() =>
+        bulkLocations({
+          brand: {
+            name: 'Cinemex',
+            logo: 'https://www.cinemex.com/logo2.png'
+          },
+          locations: [
+            {
+              name: 'Plaza oriente',
+              latitude: 19.4574855,
+              longitude: 19.457455,
+              address: 'Esquina calle oriente 66 número 34'
+            }
+          ]
+        })
+      )
   })
   .then(() => {
     console.log('EXAMPLE: -----> Uses cases executed correctly')
